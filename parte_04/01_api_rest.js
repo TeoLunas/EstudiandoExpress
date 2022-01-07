0be2c0cdad88eb5e7fc41a392db5346dee008b49
Teobaldo Lunas <89825044+TeoLunas@users.noreply.github.com>
2022-01-07 17:40:32 -0300

    v1.2

--- parte_04/01_api_rest.js
+++ parte_04/01_api_rest.js
@@ -31,29 +31,34 @@ via los metodos HTTP POST, READ, UPDATE Y DELETE
 
 */
 
+//Creacion de servidor con express. se incluye e integra el paquete body-parser.
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 app.listen(3000, () => {
     console.log('App corriendo en puerto 3000');
 });
 
+//Arreglo con canales
 let canales = [ { nombre: 'TNT'}, {nombre: 'ESP'} ];
 
+//Ruta que devuelve el arreglo de objetos canales.
 app.get('/canales', (req, res) => {
     res.send(canales);
 });
 
+//Ruta que ingresa el objeto recibido en el cuerpo de la consulta al arreglo canales, y devuelve como respuesta al cliente el arreglo con el nuevo canal agregado.
 app.post('/canal', async(req, res) => {
     const nuevo_Canal = req.body;
     canales.push(nuevo_Canal);
     res.send(canales);
 });
 
+//Ruta que recibe como parametro el nombre del canal que se desea camiar y un payload en formato json con el nuevo nombre. Se devuelve el arreglo modificado como respuesta de esta ruta al cliente.
 app.put('/canal/:canal', async(req, res) => {
     const { canal } = req.params;
     const { nombre } = req.body;
@@ -62,6 +67,8 @@ app.put('/canal/:canal', async(req, res) => {
     res.send(canales);
 });
 
+
+//Ruta que recibe como parametro el canal que se debera filtar del arreglo de objetos. Se devuelve el arreglo modificado como respuesta de esta ruta al cliente.
 app.delete('/canal/:canal', async(req, res) => {
     const { canal } = req.params;
     canales = canales.filter( (c) => c.nombre !== canal );
